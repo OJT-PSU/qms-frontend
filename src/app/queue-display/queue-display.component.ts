75719041c1bb5c7fc1683bc24d278f7551f14c41
gillmpadon <gillbertmpadon@gmail.com>
2024-02-05 13:52:36 +0800

    Interval nalang

--- src/app/queue-display/queue-display.component.ts
+++ src/app/queue-display/queue-display.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { QueueService } from '../queue.service';
-
 @Component({
   selector: 'app-queue-display',
   templateUrl: './queue-display.component.html',
@@ -16,16 +15,24 @@ export class QueueDisplayComponent implements OnInit {
   }
 
   getData(): void {
-    // setInterval(() => {
     this.queueService.getQueueCustomer().subscribe(
       (response) => {
-        this.data = response;
-        console.log('Refresh');
+        // Sort the data based on 'queueStatus' and 'createdAt'
+        this.data = response.sort((a, b) => {
+          if (a.queueStatus !== b.queueStatus) {
+            return a.queueStatus.localeCompare(b.queueStatus);
+          } else {
+            return (
+              new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+            );
+          }
+        });
+
+        console.log(this.data);
       },
       (error) => {
         console.error('Error fetching data:', error);
       }
     );
-    // }, 15000);
   }
 }
