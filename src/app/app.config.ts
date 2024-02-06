02862000edbd8d47c2b8f1ccb65a68412feee27e
H-isaac23 <dev.isaac23@gmail.com>
2024-02-06 11:41:03 +0800

    add toast

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(withFetch()),
+    importProvidersFrom([BrowserAnimationsModule]),
   ],
 };
